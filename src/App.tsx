1e57344a27091c5e1429afaaccd43ea18d271bf4
Maslomeister <maslomeister@gmail.com>
2022-03-16 18:11:14 +0300

    Изменил входной файл

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppHeader from './components/app-header/app-header';
+import BuildBurger from './components/pages/build-burger/build-burger';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader/>
+      <BuildBurger/>
     </div>
   );
 }
