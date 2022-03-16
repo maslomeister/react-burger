55af98cb3c9db7810d2305e2b47b683d6c299af6
Maslomeister <maslomeister@gmail.com>
2022-03-16 18:14:44 +0300

    Добавил страницу "Конструктор"

---
+++ src/components/pages/build-burger/build-burger.tsx
@@ -0,0 +1,18 @@
+import buildStyles from "./build-burger.module.css";
+import BurgerIngredients from "../../burger-ingredients/burger-ingredients";
+import BurgerConstructor from "../../burger-constructor/burger-constructor";
+
+function BuildBurger() {
+  return (
+    <section className={buildStyles.row}>
+      <div className={`{buildStyles.col_left} mr-10`}>
+        <BurgerConstructor />
+      </div>
+      <div className={buildStyles.col_right}>
+        <BurgerIngredients />
+      </div>
+    </section>
+  );
+}
+
+export default BuildBurger;
