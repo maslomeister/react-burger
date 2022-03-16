f19f9c386875cc5c3c110a5abd98482749360154
Maslomeister <maslomeister@gmail.com>
2022-03-16 18:14:22 +0300

    Вынес табы в отдельный файл

---
+++ src/utils/tabs-data.js
@@ -0,0 +1,22 @@
+const Data = [
+  {
+    _id: "1",
+    name: "Булки",
+    value: "one",
+    type: "bun",
+  },
+  {
+    _id: "2",
+    name: "Соусы",
+    value: "two",
+    type: "sauce",
+  },
+  {
+    _id: "3",
+    name: "Начинки",
+    value: "three",
+    type: "main",
+  },
+];
+
+export default Data;
