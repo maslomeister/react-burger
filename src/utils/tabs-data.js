3742af2fc8402246d553516c4aadeb3bcf5fea3b
Maslomeister <maslomeister@gmail.com>
2022-03-19 02:27:11 +0300

    Поправил табы

--- src/utils/tabs-data.js
+++ src/utils/tabs-data.js
@@ -1,18 +1,18 @@
 const Data = [
   {
-    _id: "1",
+    _id: 0,
     name: "Булки",
     value: "one",
     type: "bun",
   },
   {
-    _id: "2",
+    _id: 1,
     name: "Соусы",
     value: "two",
     type: "sauce",
   },
   {
-    _id: "3",
+    _id: 2,
     name: "Начинки",
     value: "three",
     type: "main",
