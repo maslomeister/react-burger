161e95a57eac21c69de05552c149729999aa7014
Maslomeister <maslomeister@gmail.com>
2022-03-19 16:12:10 +0300

    Создал оверлей модального окна

---
+++ src/components/modal-overlay/modal-overlay.tsx
@@ -0,0 +1,28 @@
+import modalOverlayStyles from "./modal-overlay.module.css";
+import { createPortal } from "react-dom";
+
+interface modalOverlayProps {
+  children: JSX.Element;
+  onClose: any;
+  show: boolean;
+}
+
+function ModalOverlay(props: modalOverlayProps) {
+  if (!props.show) {
+    return null;
+  }
+
+  return createPortal(
+    <div className={modalOverlayStyles.main} onClick={props.onClose}>
+      <div
+        className={modalOverlayStyles.inner}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {props.children}
+      </div>
+    </div>,
+    document.getElementById("modal-root")!
+  );
+}
+
+export default ModalOverlay;
