bbbb8f85270b4e440b22006d75f0d3d54412405c
Maslomeister <maslomeister@gmail.com>
2022-03-20 02:43:34 +0300

    Подправил типы, модал, имена стилей

--- src/components/order-details/order-details.tsx
+++ src/components/order-details/order-details.tsx
@@ -1,52 +1,42 @@
-import {
-  CloseIcon,
-  CheckMarkIcon,
-} from "@ya.praktikum/react-developer-burger-ui-components";
+import { CheckMarkIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import checkIconBackground from "../../images/order-details/check_icon_background.svg";
-import ModalOverlay from "../../components/modal-overlay/modal-overlay";
 import Modal from "../../components/modal/modal";
 
 import orderDetailsStyles from "./order-details.module.css";
 
-interface orderDetailsProps {
+interface OrderDetailsProps {
   show: boolean;
-  onClose: Function;
+  onClose: () => void;
 }
 
-function OrderDetails(props: orderDetailsProps) {
+function OrderDetails(props: OrderDetailsProps) {
   return (
-    <ModalOverlay onClose={props.onClose} show={props.show}>
-      <Modal
-        onClose={props.onClose}
-        title=""
-        closeIcon={<CloseIcon type="primary" />}
-      >
-        <div className={orderDetailsStyles.main}>
-          <div
-            className={`${orderDetailsStyles.order_number} text text_type_digits-large mt-30`}
-          >
-            034536
-          </div>
-          <div className="text text_type_digits-small mt-8">
-            идентификатор заказа
-          </div>
-          <div
-            className={`${orderDetailsStyles.background} mb-15 mt-15`}
-            style={{ background: `url(${checkIconBackground})` }}
-          >
-            <div className={orderDetailsStyles.check_icon_background}>
-              <CheckMarkIcon type="primary" />
-            </div>
-          </div>
-          <p className="text text_type_digits-small mb-2">
-            Ваш заказ начали готовить
-          </p>
-          <p className="text text_type_digits-small text_color_inactive mb-30">
-            Дождитесь готовности на орбитальной станции
-          </p>
+    <Modal onClose={props.onClose} show={props.show} closeIconType={"primary"}>
+      <div className={orderDetailsStyles.Main}>
+        <div
+          className={`${orderDetailsStyles.Main_orderNumberShadow} text text_type_digits-large mt-30`}
+        >
+          034536
         </div>
-      </Modal>
-    </ModalOverlay>
+        <div className="text text_type_digits-small mt-8">
+          идентификатор заказа
+        </div>
+        <div
+          className={`${orderDetailsStyles.Main_background} mb-15 mt-15`}
+          style={{ background: `url(${checkIconBackground})` }}
+        >
+          <div className={orderDetailsStyles.Main_checkMarkIconBackground}>
+            <CheckMarkIcon type="primary" />
+          </div>
+        </div>
+        <p className="text text_type_digits-small mb-2">
+          Ваш заказ начали готовить
+        </p>
+        <p className="text text_type_digits-small text_color_inactive mb-30">
+          Дождитесь готовности на орбитальной станции
+        </p>
+      </div>
+    </Modal>
   );
 }
 
