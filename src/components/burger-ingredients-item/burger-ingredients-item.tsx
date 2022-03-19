b6f5dc86948e035d802a947e6e39005533b80e3b
Maslomeister <maslomeister@gmail.com>
2022-03-20 02:41:03 +0300

    Убрал проптайпсы

--- src/components/burger-ingredients-item/burger-ingredients-item.tsx
+++ src/components/burger-ingredients-item/burger-ingredients-item.tsx
@@ -7,34 +7,29 @@ import {
 
 import itemStyles from "./burger-ingredients-item.module.css";
 
-const propTypes = {
-  imageSrc: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
-  onClick: PropTypes.func.isRequired,
-};
+interface BurgerIngredientPropTypes {
+  imageSrc: string;
+  name: string;
+  price: number;
+  onClick: () => void;
+}
 
-BurgerIngredientItem.propTypes = propTypes;
-
-//Используется для того чтобы TS автоматически получил типы которые мы указали через prop-types
-type burgerIngredientsPropTypes = PropTypes.InferProps<typeof propTypes>;
-
-function BurgerIngredientItem(props: burgerIngredientsPropTypes) {
+function BurgerIngredientItem(props: BurgerIngredientPropTypes) {
   return (
     <div
-      className={`${itemStyles.item} ml-4 mr-5 mb-10 mt-6`}
+      className={`${itemStyles.Item} ml-4 mr-5 mb-10 mt-6`}
       onClick={props.onClick}
     >
       <img
-        className={`${itemStyles.item} ml-4 mr-5`}
+        className={`${itemStyles.Item} ml-4 mr-5`}
         alt="previewImage"
         src={props.imageSrc}
       />
-      <div className={`${itemStyles.price} mb-1 mt-1`}>
+      <div className={`${itemStyles.Item_price} mb-1 mt-1`}>
         <p className="text text_type_digits-small mr-2">{props.price}</p>
         <CurrencyIcon type="primary" />
       </div>
-      <p className={`${itemStyles.name} text text_type_main-small`}>
+      <p className={`${itemStyles.Item_name} text text_type_main-small`}>
         {props.name}
       </p>
       <Counter count={1} size="default" />
