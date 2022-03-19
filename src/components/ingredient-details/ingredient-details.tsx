ef2aa393f3ce6eef042aed19d147c0f616f6b2c9
Maslomeister <maslomeister@gmail.com>
2022-03-20 02:41:30 +0300

    Подправил провеку типов

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,12 +1,10 @@
-import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
-import ModalOverlay from "../../components/modal-overlay/modal-overlay";
 import Modal from "../../components/modal/modal";
 
 import ingredientDetailsStyles from "./ingredient-details.module.css";
 
-interface ingredientDetailsProps {
+interface IngredientDetailsProps {
   show: boolean;
-  onClose: Function;
+  onClose: () => void;
   imageSrc: string;
   name: string;
   proteins: number;
@@ -15,60 +13,59 @@ interface ingredientDetailsProps {
   calories: number;
 }
 
-function IngredientDetails(props: ingredientDetailsProps) {
+function IngredientDetails(props: IngredientDetailsProps) {
   return (
-    <ModalOverlay onClose={props.onClose} show={props.show}>
-      <Modal
-        onClose={props.onClose}
-        title="Детали ингредиента"
-        closeIcon={<CloseIcon type="primary" />}
-      >
-        <div className={ingredientDetailsStyles.main}>
-          <img alt="previewImage" src={props.imageSrc} />
-          <p
-            className={`${ingredientDetailsStyles.name} text text_type_main-medium mt-4`}
-          >
-            {props.name}
-          </p>
-          <div className={`${ingredientDetailsStyles.nutrition} mb-15 mt-8`}>
-            <div className={ingredientDetailsStyles.item}>
-              <p
-                className={`${ingredientDetailsStyles.text} text text_type_main-small text_color_inactive`}
-              >
-                Калории, ккал
-              </p>
-              <p className="text text_type_digits-default text_color_inactive">
-                {props.calories}
-              </p>
-            </div>
-            <div className={ingredientDetailsStyles.item}>
-              <p className="text text_type_main-small text_color_inactive">
-                Белки, г
-              </p>
-              <p className="text text_type_digits-default text_color_inactive">
-                {props.proteins}
-              </p>
-            </div>
-            <div className={ingredientDetailsStyles.item}>
-              <p className="text text_type_main-small text_color_inactive">
-                Жиры, г
-              </p>
-              <p className="text text_type_digits-default text_color_inactive">
-                {props.fat}
-              </p>
-            </div>
-            <div className={ingredientDetailsStyles.item}>
-              <p className="text text_type_main-small text_color_inactive">
-                Углеводы, г
-              </p>
-              <p className="text text_type_digits-default text_color_inactive">
-                {props.carbohydrates}
-              </p>
-            </div>
+    <Modal
+      onClose={props.onClose}
+      show={props.show}
+      title="Детали ингредиента"
+      closeIconType="primary"
+    >
+      <div className={ingredientDetailsStyles.Main}>
+        <img alt="previewImage" src={props.imageSrc} />
+        <p
+          className={`${ingredientDetailsStyles.Main_name} text text_type_main-medium mt-4`}
+        >
+          {props.name}
+        </p>
+        <div className={`${ingredientDetailsStyles.Main_nutrition} mb-15 mt-8`}>
+          <div className={ingredientDetailsStyles.Main_item}>
+            <p
+              className={`${ingredientDetailsStyles.text} text text_type_main-small text_color_inactive`}
+            >
+              Калории, ккал
+            </p>
+            <p className="text text_type_digits-default text_color_inactive">
+              {props.calories}
+            </p>
+          </div>
+          <div className={ingredientDetailsStyles.Main_item}>
+            <p className="text text_type_main-small text_color_inactive">
+              Белки, г
+            </p>
+            <p className="text text_type_digits-default text_color_inactive">
+              {props.proteins}
+            </p>
+          </div>
+          <div className={ingredientDetailsStyles.Main_item}>
+            <p className="text text_type_main-small text_color_inactive">
+              Жиры, г
+            </p>
+            <p className="text text_type_digits-default text_color_inactive">
+              {props.fat}
+            </p>
+          </div>
+          <div className={ingredientDetailsStyles.Main_item}>
+            <p className="text text_type_main-small text_color_inactive">
+              Углеводы, г
+            </p>
+            <p className="text text_type_digits-default text_color_inactive">
+              {props.carbohydrates}
+            </p>
           </div>
         </div>
-      </Modal>
-    </ModalOverlay>
+      </div>
+    </Modal>
   );
 }
 
