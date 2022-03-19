bd7fd0e0fb1231f1857054e01fbf748193e8f31c
Maslomeister <maslomeister@gmail.com>
2022-03-19 03:42:32 +0300

    Мелкие фиксы

--- src/utils/tabs-data.js
+++ src/utils/tabs-data.js
@@ -1,18 +1,18 @@
 const Data = [
   {
-    _id: "1",
+    _id: 1,
     name: "Булки",
     value: "one",
     type: "bun",
   },
   {
-    _id: "2",
+    _id: 2,
     name: "Соусы",
     value: "two",
     type: "sauce",
   },
   {
-    _id: "3",
+    _id: 3,
     name: "Начинки",
     value: "three",
     type: "main",
