a47b2d8718869ea0581043275485cad06ba97194
Maslomeister <maslomeister@gmail.com>
2022-03-19 18:47:47 +0300

    Добавил компонент детали заказа

--- src/components/order-details/order-details.tsx
+++ src/components/order-details/order-details.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import orderDetailsStyles from "./order-details.module.css";
 import ModalOverlay from "../../components/modal-overlay/modal-overlay";
 import Modal from "../../components/modal/modal";
-import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import {
+  CloseIcon,
+  CheckMarkIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import checkIconBackground from "../../images/order-details/check_icon_background.svg";
 
 interface orderDetailsProps {
   show: boolean;
@@ -18,7 +22,28 @@ function OrderDetails(props: orderDetailsProps) {
         closeIcon={<CloseIcon type="primary" />}
       >
         <div className={orderDetailsStyles.main}>
-          <div className="text text_type_digits-large mt-30">034536</div>
+          <div
+            className={`${orderDetailsStyles.order_number} text text_type_digits-large mt-30`}
+          >
+            034536
+          </div>
+          <div className="text text_type_digits-small mt-8">
+            идентификатор заказа
+          </div>
+          <div
+            className={`${orderDetailsStyles.background} mb-15 mt-15`}
+            style={{ background: `url(${checkIconBackground})` }}
+          >
+            <div className={orderDetailsStyles.check_icon_background}>
+              <CheckMarkIcon type="primary" />
+            </div>
+          </div>
+          <p className="text text_type_digits-small mb-2">
+            Ваш заказ начали готовить
+          </p>
+          <p className="text text_type_digits-small text_color_inactive mb-30">
+            Дождитесь готовности на орбитальной станции
+          </p>
         </div>
       </Modal>
     </ModalOverlay>
