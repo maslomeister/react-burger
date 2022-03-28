f3a835967bc5213bb091e813524e5a045b85f2fd
Maslomeister <maslomeister@gmail.com>
2022-03-28 08:35:14 +0300

    Добавил параметры для модала заказа

--- src/components/order-details/order-details.tsx
+++ src/components/order-details/order-details.tsx
@@ -1,48 +1,85 @@
-import { motion } from "framer-motion";
-
 import { CheckMarkIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import AnimatedLoading from "../../components/animated-loading/animated-loading";
-import checkIconBackground from "../../assets/images/order-details/check_icon_background.svg";
 import Modal from "../../components/modal/modal";
 
 import orderDetailsStyles from "./order-details.module.css";
 
 interface OrderDetailsProps {
   show: boolean;
+  isLoading: boolean;
+  hasError: boolean;
+  error: string;
+  orderId: number;
   onClose: () => void;
 }
 
-function OrderDetails({ onClose, show }: OrderDetailsProps) {
+function OrderDetails({
+  onClose,
+  isLoading,
+  hasError,
+  error,
+  show,
+  orderId,
+}: OrderDetailsProps) {
   return (
     <Modal onClose={onClose} show={show} closeIconType="primary">
-      <div className={orderDetailsStyles["order-details"]}>
-        <div
-          className={`${orderDetailsStyles["order-number_shadow"]} text text_type_digits-large mt-30`}
-        >
-          034536
-        </div>
-
-        <div className="text text_type_digits-small mt-8">
-          идентификатор заказа
-        </div>
-
-        <div
-          className={`${orderDetailsStyles["check-mark-icon_background-size"]} mb-15 mt-15`}
-        >
-          <div className={orderDetailsStyles["check-mark-icon_size"]}>
-            <CheckMarkIcon type="primary" />
+      {!isLoading && !hasError ? (
+        <div className={orderDetailsStyles["order-details"]}>
+          <div
+            className={`${orderDetailsStyles["order-number_shadow"]} text text_type_digits-large mt-30`}
+          >
+            {orderId}
           </div>
-          <AnimatedLoading />
+
+          <div className="text text_type_digits-small mt-8">
+            идентификатор заказа
+          </div>
+
+          <div
+            className={`${orderDetailsStyles["check-mark-icon_background-size"]} mb-15 mt-15`}
+          >
+            <div className={orderDetailsStyles["check-mark-icon_size"]}>
+              <CheckMarkIcon type="primary" />
+            </div>
+            <AnimatedLoading />
+          </div>
+
+          <p className="text text_type_digits-small mb-2">
+            Ваш заказ начали готовить
+          </p>
+
+          <p className="text text_type_digits-small text_color_inactive mb-30">
+            Дождитесь готовности на орбитальной станции
+          </p>
         </div>
-
-        <p className="text text_type_digits-small mb-2">
-          Ваш заказ начали готовить
-        </p>
-
-        <p className="text text_type_digits-small text_color_inactive mb-30">
-          Дождитесь готовности на орбитальной станции
-        </p>
-      </div>
+      ) : (
+        <div className={orderDetailsStyles["order-details"]}>
+          <div
+            className={`${orderDetailsStyles["order-number_shadow"]} text text_type_main-large mt-30`}
+          >
+            Загрузка
+          </div>
+          <div
+            className={`${orderDetailsStyles["check-mark-icon_background-size"]} mb-15 mt-15`}
+          >
+            <AnimatedLoading />
+          </div>
+        </div>
+      )}
+      {hasError && (
+        <div className={orderDetailsStyles["order-details"]}>
+          <div
+            className={`${orderDetailsStyles["order-number_shadow"]} text text_type_main-large mt-30`}
+          >
+            Произошла ошибка
+          </div>
+          <div
+            className={`${orderDetailsStyles["order-number_shadow"]} text text_type_main-medium mb-15 mt-15`}
+          >
+            {error}
+          </div>
+        </div>
+      )}
     </Modal>
   );
 }
