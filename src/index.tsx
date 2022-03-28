87daafa3f165b6188add2cba717573012cf7c750
Maslomeister <maslomeister@gmail.com>
2022-03-28 05:29:03 +0300

    Добавил роутер

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./components/app/app";
 import reportWebVitals from "./reportWebVitals";
 
+import "./index.css";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
