c917379b1bd3d0481a0f55d6db6e99f4eece7435
Maslomeister <maslomeister@gmail.com>
2022-03-28 05:22:04 +0300

    Вынес код в одельный компонент

--- src/components/order-details/order-details.tsx
+++ src/components/order-details/order-details.tsx
@@ -1,4 +1,7 @@
+import { motion } from "framer-motion";
+
 import { CheckMarkIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import AnimatedLoading from "../../components/animated-loading/animated-loading";
 import checkIconBackground from "../../assets/images/order-details/check_icon_background.svg";
 import Modal from "../../components/modal/modal";
 
@@ -9,29 +12,33 @@ interface OrderDetailsProps {
   onClose: () => void;
 }
 
-function OrderDetails(props: OrderDetailsProps) {
+function OrderDetails({ onClose, show }: OrderDetailsProps) {
   return (
-    <Modal onClose={props.onClose} show={props.show} closeIconType="primary">
+    <Modal onClose={onClose} show={show} closeIconType="primary">
       <div className={orderDetailsStyles["order-details"]}>
         <div
           className={`${orderDetailsStyles["order-number_shadow"]} text text_type_digits-large mt-30`}
         >
           034536
         </div>
+
         <div className="text text_type_digits-small mt-8">
           идентификатор заказа
         </div>
+
         <div
           className={`${orderDetailsStyles["check-mark-icon_background-size"]} mb-15 mt-15`}
-          style={{ background: `url(${checkIconBackground})` }}
         >
-          <div className={orderDetailsStyles["check-mark-icon_background"]}>
+          <div className={orderDetailsStyles["check-mark-icon_size"]}>
             <CheckMarkIcon type="primary" />
           </div>
+          <AnimatedLoading />
         </div>
+
         <p className="text text_type_digits-small mb-2">
           Ваш заказ начали готовить
         </p>
+
         <p className="text text_type_digits-small text_color_inactive mb-30">
           Дождитесь готовности на орбитальной станции
         </p>
