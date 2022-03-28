1c7f6824bbe8b11ad5f56f4c865e0cfaca2b2d34
Maslomeister <maslomeister@gmail.com>
2022-03-28 08:32:15 +0300

    Создал редюсер

---
+++ src/components/reducers/reducers.ts
@@ -0,0 +1,64 @@
+interface IngredientType {
+  _id: string;
+  image: string;
+  text: string;
+  price: number;
+}
+
+interface InitialStateType {
+  ingredients: IngredientType[];
+  bun: IngredientType;
+  totalPrice: number;
+}
+
+interface ActionType {
+  type: string;
+  payload: any;
+}
+
+export const burgerConstructorReducer = (state: InitialStateType, action: ActionType):InitialStateType => {
+  switch (action.type) {
+    case "ADD_BUN":{
+      let newState = {
+        ...state,
+        bun: {
+          _id: action.payload._id,
+          image: action.payload.image,
+          text: action.payload.text,
+          price: action.payload.price,
+        },
+      }
+
+      let newTotal = newState.bun.price * 2;
+      newState.totalPrice += newTotal;
+      return newState;
+    }
+    case "ADD_INGREDIENT": {
+      let newState =  {...state, ingredients: [
+        ...state.ingredients,
+        action.payload.newElem
+      ]}
+      
+      newState.totalPrice +=  action.payload.newElem.price;
+      return newState ; 
+    }
+    case "REMOVE_INGREDIENT":
+      let ingredient = state.ingredients.filter(item => item._id === action.payload._id)
+      let filteredIngredients = state.ingredients.filter(item => item._id !== action.payload._id)
+
+      let newState =  {
+        ...state, 
+        ingredients: filteredIngredients
+      }
+
+      let newTotal = state.totalPrice;
+      newTotal -= ingredient[0].price;
+      
+      newState.totalPrice = newTotal;
+      return newState ; 
+    default:{
+      throw new Error(`Wrong type of action: ${action.type}`);
+    }
+      
+  }
+}
