a0f2fdddd958530b10d5bcf3b781b4169621439e
Maslomeister <maslomeister@gmail.com>
2022-03-28 19:38:10 +0300

    Добавил проверку активности страницы в навлинк

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -9,22 +9,24 @@ import {
 
 import headerStyles from "./app-header.module.css";
 
+const setActive = ({ isActive }: { isActive: Boolean }) =>
+  "text text_type_main-default ml-2 " +
+  (isActive ? headerStyles["menu__item_active"] : headerStyles["menu__item"]);
+
 function AppHeader() {
   return (
     <header className={headerStyles["header"]}>
       <nav className={headerStyles["header-inner"]}>
         <ul className={`${headerStyles["menu-left"]} mb-4 mt-4`}>
-          <li
-            className={`${headerStyles["menu__item_active"]} ml-5 mr-5 mb-5 mt-5`}
-          >
+          <li className={`${headerStyles["menu__item"]} ml-5 mr-5 mb-5 mt-5`}>
             <BurgerIcon type="primary" />
-            <NavLink className="text text_type_main-default ml-2" to="/">
+            <NavLink className={setActive} to="/">
               Конструктор
             </NavLink>
           </li>
           <li className={`${headerStyles["menu__item"]} ml-5 mr-5 mb-5 mt-5`}>
             <ListIcon type="primary" />
-            <NavLink className="text text_type_main-default ml-2" to="/orders">
+            <NavLink className={setActive} to="/orders">
               Лента заказов
             </NavLink>
           </li>
@@ -35,7 +37,7 @@ function AppHeader() {
         <ul className={`${headerStyles["menu-right"]} mb-4 mt-4`}>
           <li className={`${headerStyles["menu__item"]} ml-5 mr-5 mb-5 mt-5`}>
             <ProfileIcon type="primary" />
-            <NavLink className="text text_type_main-default ml-2" to="/account">
+            <NavLink className={setActive} to="/account">
               Личный кабинет
             </NavLink>
           </li>
