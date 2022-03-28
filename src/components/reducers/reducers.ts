e826f37fffdb960b09d653eac5e005cfc77ed3a4
Maslomeister <maslomeister@gmail.com>
2022-03-28 23:54:39 +0300

    Наследование типов

--- src/components/reducers/reducers.ts
+++ src/components/reducers/reducers.ts
@@ -1,36 +1,4 @@
-type Only<T, U> = {
-  [P in keyof T]: T[P];
-} & {
-  [P in keyof U]: never;
-};
-
-type Either<T, U> = Only<T, U> | Only<U, T>;
-
-type RequireAtLeastOne<T, Keys extends keyof T = keyof T> =
-    Pick<T, Exclude<keyof T, Keys>> 
-    & {
-        [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>
-    }[Keys]
-
-interface IngredientType {
-  _id: string;
-  image: string;
-  text: string;
-  price: number;
-}
-
-interface InitialStateType {
-  ingredients: IngredientType[];
-  bun: IngredientType;
-  totalPrice: number;
-}
-
-interface PayloadObject {
-  _id: string;
-  image: string;
-  text: string;
-  price: number;
-}
+import {InitialStateType, PayloadObject } from "../services/appContext";
 
 interface ActionType {
   type: ("ADD_BUN" | "ADD_INGREDIENT" | "REMOVE_INGREDIENT");
@@ -40,43 +8,57 @@ interface ActionType {
 export const burgerConstructorReducer = (state: InitialStateType, action: ActionType):InitialStateType => {
   switch (action.type) {
     case "ADD_BUN":{
-      let newState = {
-        ...state,
-        bun: {
-          _id: action.payload._id,
-          image: action.payload.image,
-          text: action.payload.text,
-          price: action.payload.price,
-        },
+      if(action.payload.price && action.payload.image && action.payload.text){
+        const newState = {
+          ...state,
+          bun: {
+            _id: action.payload._id,
+            image: action.payload.image,
+            text: action.payload.text,
+            price: action.payload.price,
+          },
+          totalPrice: state.totalPrice + action.payload.price *2,
       }
-
-      const newTotal = newState.bun.price * 2;
-      newState.totalPrice += newTotal;
-      return newState;
+        return newState;
+      }else{
+        return state;
+      }
     }
     case "ADD_INGREDIENT": {
-      let newState =  {...state, ingredients: [
-        ...state.ingredients,
-        action.payload
-      ]}
+      if(action.payload.price && action.payload.image && action.payload.text){
+        const newState =  {
+          ...state, 
+          ingredients: [
+          ...state.ingredients,
+          action.payload
+          ],
+          totalPrice: state.totalPrice + action.payload.price,
+        }
+        return newState as InitialStateType ; 
+      }else{
+        return state;
+      }
       
-      newState.totalPrice +=  action.payload.price;
-      return newState ; 
     }
     case "REMOVE_INGREDIENT":
-      const ingredient = state.ingredients.filter(item => item._id === action.payload._id)
-      const filteredIngredients = state.ingredients.filter(item => item._id !== action.payload._id)
+      const ingredient = state.ingredients.find(item => item._id === action.payload._id)
+      try {
+        if(ingredient && ingredient.price){
+          const filteredIngredients = state.ingredients.filter(item => item._id !== action.payload._id)
 
-      let newState =  {
-        ...state, 
-        ingredients: filteredIngredients
+          const newState =  {...state, 
+            ingredients: filteredIngredients,
+            totalPrice: state.totalPrice - ingredient.price,
+          }
+          
+          return newState ; 
+        }
+
+      } catch (err) {
+        throw new Error(`Error while removing ingredient: ${err}`);
       }
-
-      let newTotal = state.totalPrice;
-      newTotal -= ingredient[0].price;
+      return state;
       
-      newState.totalPrice = newTotal;
-      return newState ; 
     default:{
       throw new Error(`Wrong type of action: ${action.type}`);
     }
