e62167da2df90a4d8cbeef2dace3f041167947f3
Maslomeister <maslomeister@gmail.com>
2022-03-28 05:26:21 +0300

    Вынес в отдельный компонент

---
+++ src/components/burger-constructor-item/burger-constructor-item.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useMotionValue, Reorder, useDragControls } from "framer-motion";
+
+import {
+  ConstructorElement,
+  DragIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+
+import constructorItemStyles from "./burger-constructor-item.module.css";
+
+type Item = {
+  _id: string;
+  image: string;
+  text: string;
+  price: number;
+};
+
+interface BurgerConstructorItemPropTypes {
+  item: Item;
+}
+
+function BurgerConstructorItem({ item }: BurgerConstructorItemPropTypes) {
+  return (
+    <div className={constructorItemStyles["ingredient"]}>
+      <div className={constructorItemStyles["_draggable"]}>
+        <DragIcon type="primary" />
+      </div>
+
+      <div className={constructorItemStyles["constructor-element-wrapper"]}>
+        <ConstructorElement
+          text={item.text}
+          price={item.price}
+          thumbnail={item.image}
+        />
+      </div>
+    </div>
+  );
+}
+export default BurgerConstructorItem;
