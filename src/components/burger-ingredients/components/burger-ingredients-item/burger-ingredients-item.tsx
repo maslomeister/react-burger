6f7b2233b5d32a374f2ebbde2fabdb4a96497aef
Maslomeister <maslomeister@gmail.com>
2022-04-04 20:20:24 +0300

    refactor

--- src/components/burger-ingredients/components/burger-ingredients-item/burger-ingredients-item.tsx
+++ src/components/burger-ingredients/components/burger-ingredients-item/burger-ingredients-item.tsx
@@ -5,23 +5,9 @@ import { useDrag } from "react-dnd";
 import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import { imageMotion, textMotion } from "./motion-config";
 import IngredientCounter from "./ingredient-counter/ingredient-counter";
+import { Ingredient } from "../../../../utils/burger-api";
 
 import itemStyles from "./burger-ingredients-item.module.css";
-
-interface Ingredient {
-  _id: string;
-  name: string;
-  type: string;
-  proteins: number;
-  fat: number;
-  carbohydrates: number;
-  calories: number;
-  price: number;
-  image: string;
-  image_mobile: string;
-  image_large: string;
-  __v: number;
-}
 
 interface BurgerIngredientItemPropTypes {
   ingredient: Ingredient;
