bd186991453a941a74736b4093b0de88498a9f71
Maslomeister <maslomeister@gmail.com>
2022-04-05 12:18:19 +0300

    feat: Смена цвета иконки в навбаре

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 import {
@@ -13,20 +14,35 @@ const setActive = ({ isActive }: { isActive: Boolean }) =>
   "text text_type_main-default ml-2 " +
   (isActive ? headerStyles["menu__item_active"] : headerStyles["menu__item"]);
 
+//Наверное можно лучше
+const buttonType = (link: string, active: string) => {
+  if (active === link) return "primary";
+  return "secondary";
+};
+
 function AppHeader() {
+  const [activeLink, setActiveLink] = useState("");
   return (
     <header className={headerStyles["header"]}>
       <nav className={headerStyles["header-inner"]}>
         <ul className={`${headerStyles["menu-left"]} mb-4 mt-4`}>
           <li className={`${headerStyles["menu__item"]} ml-5 mr-5 mb-5 mt-5`}>
-            <BurgerIcon type="primary" />
-            <NavLink className={setActive} to="/">
+            <NavLink
+              className={setActive}
+              to="/"
+              onClick={() => setActiveLink("1")}
+            >
+              <BurgerIcon type={buttonType(activeLink, "1")} />
               Конструктор
             </NavLink>
           </li>
           <li className={`${headerStyles["menu__item"]} ml-5 mr-5 mb-5 mt-5`}>
-            <ListIcon type="primary" />
-            <NavLink className={setActive} to="/orders">
+            <ListIcon type={buttonType(activeLink, "2")} />
+            <NavLink
+              className={setActive}
+              to="/orders"
+              onClick={() => setActiveLink("2")}
+            >
               Лента заказов
             </NavLink>
           </li>
@@ -36,8 +52,12 @@ function AppHeader() {
         </div>
         <ul className={`${headerStyles["menu-right"]} mb-4 mt-4`}>
           <li className={`${headerStyles["menu__item"]} ml-5 mr-5 mb-5 mt-5`}>
-            <ProfileIcon type="primary" />
-            <NavLink className={setActive} to="/account">
+            <ProfileIcon type={buttonType(activeLink, "3")} />
+            <NavLink
+              className={setActive}
+              to="/account"
+              onClick={() => setActiveLink("3")}
+            >
               Личный кабинет
             </NavLink>
           </li>
