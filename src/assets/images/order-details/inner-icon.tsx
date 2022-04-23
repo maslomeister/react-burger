1c439c13b2cc656c29ee0e167e46f162350dd694
Maslomeister <maslomeister@gmail.com>
2022-04-24 01:24:33 +0300

    refactor: Переработал иконки загрузки

---
+++ src/assets/images/order-details/inner-icon.tsx
@@ -0,0 +1,36 @@
+interface Types {
+  width?: number;
+  height?: number;
+}
+
+export function InnerIcon({ width = 68, height = 66 }: Types) {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 68 66"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M24.9249 2.91091C30.3362 -0.970307 37.6638 -0.970302 43.0751 2.91092L61.6345 16.2224C67.0459 20.1036 69.3102 26.9832 67.2433 33.2632L60.1542 54.8016C58.0873 61.0815 52.1592 65.3333 45.4703 65.3333H22.5296C15.8408 65.3333 9.91274 61.0815 7.84578 54.8016L0.756717 33.2632C-1.31024 26.9832 0.954096 20.1036 6.36546 16.2224L24.9249 2.91091Z"
+        fill="url(#paint0_radial_18254_893)"
+        fillOpacity="0.25"
+      />
+      <defs>
+        <radialGradient
+          id="paint0_radial_18254_893"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(34 32.6667) rotate(136.146) scale(47.1499 35.4679)"
+        >
+          <stop stopColor="#801AB3" stopOpacity="0" />
+          <stop offset="1" stopColor="#4C4CFF" stopOpacity="0.5" />
+        </radialGradient>
+      </defs>
+    </svg>
+  );
+}
