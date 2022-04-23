9dc67cc14ce3781823258a13f2fecd45e1d693c2
Maslomeister <maslomeister@gmail.com>
2022-04-24 01:24:52 +0300

    refactor: Переработал иконки загрузки

--- src/components/animated-loading/animated-loading.tsx
+++ src/components/animated-loading/animated-loading.tsx
@@ -1,19 +1,40 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-import checkIconBackgroundOuter from "../../assets/images/order-details/outer-icon.svg";
-import checkIconBackgroundMiddle from "../../assets/images/order-details/middle-icon.svg";
-import checkIconBackgroundInner from "../../assets/images/order-details/inner-icon.svg";
+import { OuterIcon } from "../../assets/images/order-details/outer-icon";
+import { MiddleIcon } from "../../assets/images/order-details/middle-icon";
+import { InnerIcon } from "../../assets/images/order-details/inner-icon";
 
 import styles from "./animated-loading.module.css";
 
-export default function AnimatedLoading() {
+export function AnimatedLoading({
+  size = "small",
+}: {
+  size?: "small" | "medium" | "large";
+}) {
+  let mediumIconDimensions = { width: 0, height: 0 };
+  let outerIconDimensions = { width: 0, height: 0 };
+  let innerIconDimensions = { width: 0, height: 0 };
+  if (size) {
+    switch (size) {
+      case "small": {
+        innerIconDimensions = { width: 68, height: 66 };
+        mediumIconDimensions = { width: 98, height: 102 };
+        outerIconDimensions = { width: 107, height: 100 };
+        break;
+      }
+      case "medium": {
+        innerIconDimensions = { width: 200, height: 194 };
+        mediumIconDimensions = { width: 250, height: 260 };
+        outerIconDimensions = { width: 300, height: 280 };
+        break;
+      }
+    }
+  }
   return (
     <>
-      <motion.img
+      <motion.div
         className={styles["check-mark-icon_background-center"]}
-        src={checkIconBackgroundOuter}
-        alt="outerBackground"
         initial={{ rotate: 0 }}
         animate={{ rotate: -360 }}
         transition={{
@@ -23,11 +44,14 @@ export default function AnimatedLoading() {
             repeat: Infinity,
           },
         }}
-      />
-      <motion.img
+      >
+        <OuterIcon
+          width={outerIconDimensions.width}
+          height={outerIconDimensions.height}
+        />
+      </motion.div>
+      <motion.div
         className={styles["check-mark-icon_background-center"]}
-        src={checkIconBackgroundMiddle}
-        alt="middleBackground"
         initial={{ rotate: 0 }}
         animate={{ rotate: 360 }}
         transition={{
@@ -37,11 +61,14 @@ export default function AnimatedLoading() {
             repeat: Infinity,
           },
         }}
-      />
-      <motion.img
+      >
+        <MiddleIcon
+          width={mediumIconDimensions.width}
+          height={mediumIconDimensions.height}
+        />
+      </motion.div>
+      <motion.div
         className={styles["check-mark-icon_background-center"]}
-        src={checkIconBackgroundInner}
-        alt="innerBackground"
         initial={{ rotate: 0 }}
         animate={{ rotate: -360 }}
         transition={{
@@ -51,7 +78,12 @@ export default function AnimatedLoading() {
             repeat: Infinity,
           },
         }}
-      />
+      >
+        <InnerIcon
+          width={innerIconDimensions.width}
+          height={innerIconDimensions.height}
+        />
+      </motion.div>
     </>
   );
 }
