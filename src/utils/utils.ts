aae0fb29a9bd238b714718f3cfabb8113a45f95d
Maslomeister <maslomeister@gmail.com>
2022-04-24 01:33:21 +0300

    feat: Работа с куками

---
+++ src/utils/utils.ts
@@ -0,0 +1,32 @@
+export function getCookie(name: string) {
+  const matches = document.cookie.match(
+    new RegExp(
+      "(?:^|; )" + name.replace(/([.$?*|{}()[\]\\/+^])/g, "\\$1") + "=([^;]*)"
+    )
+  );
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+export function setCookie(name: string, value: string, props?: any) {
+  console.log(props);
+  props = props || {};
+  let exp = props.expires;
+  if (typeof exp == "number" && exp) {
+    const d = new Date();
+    d.setTime(d.getTime() + exp * 1000);
+    exp = props.expires = d;
+  }
+  if (exp && exp.toUTCString) {
+    props.expires = exp.toUTCString();
+  }
+  value = encodeURIComponent(value);
+  let updatedCookie = name + "=" + value;
+  for (const propName in props) {
+    updatedCookie += "; " + propName;
+    const propValue = props[propName];
+    if (propValue !== true) {
+      updatedCookie += "=" + propValue;
+    }
+  }
+  document.cookie = updatedCookie;
+}
