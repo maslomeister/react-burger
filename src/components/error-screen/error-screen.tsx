8063451a9a47da88f3845e15508c9adfd5cdd94e
Maslomeister <maslomeister@gmail.com>
2022-04-25 18:40:09 +0300

    feat: Экран ошибки

---
+++ src/components/error-screen/error-screen.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+import styles from "./loading-screen.module.css";
+
+export function ErrorScreen({ text }: { text: string }) {
+  return (
+    <AnimatePresence>
+      <motion.div className={styles["container"]}>
+        <p
+          className={`${styles["loading-text"]} text text_type_main-large mb-5`}
+        >
+          {text}
+        </p>
+      </motion.div>
+    </AnimatePresence>
+  );
+}
