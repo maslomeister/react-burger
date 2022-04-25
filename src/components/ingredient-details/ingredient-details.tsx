03f85cf89ce9d5fab535fbe28f6850bb40fab542
Maslomeister <maslomeister@gmail.com>
2022-04-25 05:02:19 +0300

    refactor

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,16 +1,11 @@
 import { motion } from "framer-motion";
 
-import { Modal } from "../../components/modal/modal";
 import { NutritionItem } from "./components/nutrition-item/nutrition-item";
 import { useAppSelector } from "../../services/hooks";
 
 import style from "./ingredient-details.module.css";
 
-interface IngredientDetailsTypes {
-  onClose: () => void;
-}
-
-export function IngredientDetails({ onClose }: IngredientDetailsTypes) {
+export function IngredientDetails() {
   const {
     modalImage,
     modalName,
@@ -21,32 +16,30 @@ export function IngredientDetails({ onClose }: IngredientDetailsTypes) {
   } = useAppSelector((state) => state.ingredientDetails);
 
   return (
-    <Modal onClose={onClose} title="Детали ингредиента" closeIconType="primary">
-      <div className={style["ingredient-details"]}>
-        <motion.img
-          alt="previewImage"
-          src={modalImage}
-          initial={{ rotate: -2 }}
-          animate={{ rotate: 2 }}
-          transition={{
-            duration: 1,
-            ease: "easeInOut",
-            repeat: Infinity,
-            repeatType: "mirror",
-          }}
-        />
-        <p
-          className={`${style["ingredient-details__text"]} text text_type_main-medium mt-4`}
-        >
-          {modalName}
-        </p>
-        <div className={`${style["ingredient-details__nutrition"]} mb-15 mt-8`}>
-          <NutritionItem name={"Калории,ккал"} value={modalCalories} />
-          <NutritionItem name={"Белки, г"} value={modalProteins} />
-          <NutritionItem name={"Жиры, г"} value={modalFat} />
-          <NutritionItem name={"Углеводы, г"} value={modalCarbohydrates} />
-        </div>
+    <div className={style["ingredient-details"]}>
+      <motion.img
+        alt="previewImage"
+        src={modalImage}
+        initial={{ rotate: -2 }}
+        animate={{ rotate: 2 }}
+        transition={{
+          duration: 1,
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatType: "mirror",
+        }}
+      />
+      <p
+        className={`${style["ingredient-details__text"]} text text_type_main-medium mt-4`}
+      >
+        {modalName}
+      </p>
+      <div className={`${style["ingredient-details__nutrition"]} mb-15 mt-8`}>
+        <NutritionItem name={"Калории,ккал"} value={modalCalories} />
+        <NutritionItem name={"Белки, г"} value={modalProteins} />
+        <NutritionItem name={"Жиры, г"} value={modalFat} />
+        <NutritionItem name={"Углеводы, г"} value={modalCarbohydrates} />
       </div>
-    </Modal>
+    </div>
   );
 }
