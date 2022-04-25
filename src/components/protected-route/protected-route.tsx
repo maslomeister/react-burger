f2b28decb609ef0846f9f5c06a5397fa58a82c0a
Maslomeister <maslomeister@gmail.com>
2022-04-25 05:00:13 +0300

    refactor

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -1,66 +1,21 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useLocation, Navigate } from "react-router-dom";
 
 import { useAppSelector, useAppDispatch } from "../../services/hooks";
-import { getCookie } from "../../utils/utils";
-import { getNewAccessToken, getUserProfile } from "../../services/auth/auth";
+import { getCookie, userAuthorized } from "../../utils/utils";
+import {
+  getNewAccessToken,
+  getOrUpdateUserData,
+} from "../../services/auth/auth";
+import { setIdle } from "../../services/auth/auth";
 
 export function ProtectedRoute({ children }: { children: JSX.Element }) {
-  const dispatch = useAppDispatch();
-  const { name } = useAppSelector((state) => state.authUser);
+  const { user } = useAppSelector((state) => state.authUser);
   let location = useLocation();
 
-  const accessToken = getCookie("accessToken");
-
-  const getNewToken = async () => {
-    const refreshToken = getCookie("refreshToken");
-    if (!refreshToken) {
-      return;
-    }
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ token: refreshToken }),
-    };
-
-    try {
-      const originalPromiseResult = await dispatch(
-        getNewAccessToken(requestOptions)
-      ).unwrap();
-      console.log(originalPromiseResult);
-    } catch (rejectedValueOrSerializedError) {
-      // handle error here
-    }
-  };
-
-  const getUserData = async () => {
-    const requestOptions = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        authorization: accessToken,
-      },
-    };
-
-    try {
-      const originalPromiseResult = await dispatch(
-        getUserProfile(requestOptions)
-      ).unwrap();
-      console.log(originalPromiseResult);
-    } catch (rejectedValueOrSerializedError) {
-      // handle error here
-    }
-  };
-
-  useEffect(() => {
-    if (accessToken === undefined) {
-      getNewToken();
-    } else {
-      getUserData();
-    }
-  }, []);
-
-  if (name === "") {
+  if (!userAuthorized(user)) {
+    if (location.pathname === "/logout")
+      return <Navigate to="/login" replace />;
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
