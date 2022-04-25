c6ba61a9b9e477a068cd75cd128ad60ba4d1b805
Maslomeister <maslomeister@gmail.com>
2022-04-25 05:01:08 +0300

    feat: Страница ингредиента

---
+++ src/components/pages/ingredient/ingredient.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { useAppSelector, useAppDispatch } from "../../../services/hooks";
+import { IngredientDetails } from "../../../components/ingredient-details/ingredient-details";
+import { addDataToModal } from "../../../services/ingredient-details";
+
+import styles from "./ingredient.module.css";
+
+export function Ingredient() {
+  let content;
+  const dispatch = useAppDispatch();
+  const { id } = useParams();
+  const ingredients = useAppSelector(
+    (state) => state.burgerIngredients.ingredients
+  );
+  const [ingredientExists, setIngredientExists] = useState(false);
+
+  console.log(id);
+
+  useEffect(() => {
+    const ingredient = ingredients.find((obj) => obj._id === id);
+    if (ingredient) {
+      setIngredientExists(true);
+      dispatch(
+        addDataToModal({
+          modalImage: ingredient.image_large,
+          modalName: ingredient.name,
+          modalCalories: ingredient.calories,
+          modalProteins: ingredient.price,
+          modalFat: ingredient.fat,
+          modalCarbohydrates: ingredient.carbohydrates,
+        })
+      );
+    }
+  }, [id]);
+
+  if (ingredientExists) {
+    content = <IngredientDetails />;
+  } else {
+    content = (
+      <p className={`${styles["text-shadow"]} text text_type_main-large`}>
+        Такого ингредиента не существует
+      </p>
+    );
+  }
+
+  return <div className={styles["container"]}>{content}</div>;
+}
