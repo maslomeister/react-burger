17d1be8af7b277b0a95b7b378691699483e6e8a6
Maslomeister <maslomeister@gmail.com>
2022-05-26 13:28:45 +0300

    feat: Склонение слов

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -105,3 +105,18 @@ export function validatePassword(input: string) {
   }
   return { isValid: true, error: "" };
 }
+
+export function declOfNum(n: number, text_forms: string[]) {
+  n = Math.abs(n) % 100;
+  var n1 = n % 10;
+  if (n > 10 && n < 20) {
+    return text_forms[2];
+  }
+  if (n1 > 1 && n1 < 5) {
+    return text_forms[1];
+  }
+  if (n1 == 1) {
+    return text_forms[0];
+  }
+  return text_forms[2];
+}
