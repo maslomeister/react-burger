9d7d40905e65e25631ba03207d43ffe5d0014881
Maslomeister <maslomeister@gmail.com>
2022-05-31 14:21:10 +0300

    feat: Функции помощники

---
+++ src/utils/helpers.ts
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 } from "uuid";
+
+export function generateIngredientsFromIds(
+  allIngredients: Array<IIngredient>,
+  orderData: IOrder
+): Array<IIngredient> {
+  return orderData.ingredients.map((ingredient) => {
+    const foundIngredient = allIngredients.filter(
+      (item) => item._id === ingredient
+    )[0];
+
+    const newObject = { ...foundIngredient, _uniqueId: uuidv4() };
+    return newObject;
+  });
+}
+
+export function getTotalPriceOfIngredients(
+  ingredients: Array<IIngredient>
+): number {
+  return ingredients.reduce((acc, obj) => {
+    if (obj.type === "bun") {
+      return acc + obj.price * 2;
+    } else return acc + obj.price;
+  }, 0);
+}
