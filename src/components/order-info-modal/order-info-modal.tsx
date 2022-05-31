edc21f3f5019bbc6066718061afb914d4f8c3bb9
Maslomeister <maslomeister@gmail.com>
2022-05-31 14:20:33 +0300

    refactor: Реорганизовал код и вынес функции

--- src/components/order-info-modal/order-info-modal.tsx
+++ src/components/order-info-modal/order-info-modal.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { useParams } from "react-router-dom";
-import { v4 as uuidv4 } from "uuid";
 
 import { useAppSelector, useAppDispatch } from "../../services/hooks";
 import { TotalPrice } from "../../components/total-price/total-price";
@@ -8,26 +7,21 @@ import { formatDisplayDate } from "../../utils/utils";
 import { getOrderByNumber } from "../../services/reducers/order-details";
 import { LoadingScreen } from "../../components/loading-screen/loading-screen";
 import { IngredientCircleImage } from "../../components/ingredient-circle-image/ingredient-circle-image";
+import {
+  generateIngredientsFromIds,
+  getTotalPriceOfIngredients,
+} from "../../utils/helpers";
 
 import styles from "./order-info-modal.module.css";
-
-interface IOrderedIngredient {
-  type: string;
-  _id: string;
-  _uniqueId: string;
-  name: string;
-  image: string;
-  price: number;
-}
 
 export function OrderInfoModal() {
   const dispatch = useAppDispatch();
 
   const params = useParams();
 
-  const orderData = useAppSelector((state) => state.orderDetails.orderData);
+  const { orderData } = useAppSelector((state) => state.orderDetails);
 
-  const [ingredients, setIngredients] = useState<IOrderedIngredient[]>();
+  const [ingredients, setIngredients] = useState<IIngredient[]>([]);
   const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
@@ -36,106 +30,76 @@ export function OrderInfoModal() {
     }
   }, [dispatch, orderData, params]);
 
-  const _ingredients = useAppSelector(
+  const allIngredients = useAppSelector(
     (state) => state.burgerIngredients.ingredients
   );
 
-  useEffect(() => {
-    const allIngredients = orderData?.ingredients.map((ingredient) => {
-      const foundIngredient = _ingredients.find((item) => {
-        return item._id === ingredient;
-      });
+  useMemo(() => {
+    if (!orderData) return;
+    setIngredients(generateIngredientsFromIds(allIngredients, orderData));
+  }, [allIngredients, orderData]);
 
-      if (foundIngredient) {
-        return {
-          type: foundIngredient.type,
-          _id: ingredient,
-          image: foundIngredient.image_mobile,
-          name: foundIngredient.name,
-          price: foundIngredient.price,
-          _uniqueId: uuidv4(),
-        };
-      }
-      return {
-        type: "none",
-        _id: "none",
-        image: "none",
-        name: "none",
-        price: -1,
-        _uniqueId: uuidv4(),
-      };
-    });
-    setIngredients(allIngredients);
-  }, [_ingredients, orderData, orderData?.ingredients]);
-
-  useEffect(() => {
-    if (ingredients === undefined) return;
-
-    const total = ingredients.reduce((acc, obj) => {
-      if (obj.type === "bun") {
-        return acc + obj.price * 2;
-      } else return acc + obj.price;
-    }, 0);
-
-    setTotalPrice(total);
+  useMemo(() => {
+    if (!ingredients) return;
+    setTotalPrice(getTotalPriceOfIngredients(ingredients));
   }, [ingredients]);
 
-  return (
-    <>
-      {orderData && ingredients ? (
-        <div className={`${styles["container"]} ml-10 mr-10 mb-10`}>
-          <p className="text text_type_digits-default mt-15">
-            #{orderData.number}
-          </p>
+  if (!orderData || !ingredients)
+    return <LoadingScreen text="Загружаются данные заказа" size="medium" />;
 
-          <p className="text text_type_main-medium mt-10 mb-3">
-            {orderData.name}
-          </p>
-          <p
-            className={`${
-              orderData.status === "done" ? styles["text_success"] : null
-            } text text_type_main-default mb-15`}
-          >
-            {orderData.status === "done"
-              ? "Выполнен"
-              : orderData.status === "pending"
-              ? "Готовится"
-              : "Создан"}
-          </p>
-          <p className="text text_type_main-medium mb-6">Состав:</p>
-          <div className={`${styles["ingredients-container"]} mb-10`}>
-            {ingredients.map((ingredient) => {
-              return (
-                <div
-                  className={`${styles["ingredient-container"]} mb-4`}
-                  key={ingredient._uniqueId}
-                >
-                  <div className="mr-4">
-                    <IngredientCircleImage image={ingredient.image} />
-                  </div>
-                  <p
-                    className={`${styles["ingredient-title"]} text text_type_main-default`}
-                  >
-                    {ingredient.name}
-                  </p>
-                  <div className={`${styles["ingredient-price"]}`}>
-                    <TotalPrice
-                      price={ingredient.price}
-                      double={ingredient.type === "bun" ? true : undefined}
-                    />
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-          <div className={styles["order-date-price"]}>
-            <p>{formatDisplayDate(new Date(orderData.createdAt))}</p>
-            <TotalPrice price={totalPrice} />
-          </div>
-        </div>
-      ) : (
-        <LoadingScreen text="Загружаются данные заказа" size="medium" />
-      )}
-    </>
+  const ruStatus = () => {
+    switch (orderData.status) {
+      case "done":
+        return "Выполнен";
+      case "pending":
+        return "Готовится";
+      case "created":
+        return "Создан";
+    }
+  };
+
+  return (
+    <div className={`${styles["container"]} ml-10 mr-10 mb-10`}>
+      <p className="text text_type_digits-default mt-15">#{orderData.number}</p>
+
+      <p className="text text_type_main-medium mt-10 mb-3">{orderData.name}</p>
+      <p
+        className={`${
+          orderData.status === "done" ? styles["text_success"] : null
+        } text text_type_main-default mb-15`}
+      >
+        {ruStatus()}
+      </p>
+      <p className="text text_type_main-medium mb-6">Состав:</p>
+      <div className={`${styles["ingredients-container"]} mb-10`}>
+        {ingredients.map((ingredient) => {
+          return (
+            <div
+              className={`${styles["ingredient-container"]} mb-4`}
+              key={ingredient._uniqueId}
+            >
+              <div className="mr-4">
+                <IngredientCircleImage image={ingredient.image} />
+              </div>
+              <p
+                className={`${styles["ingredient-title"]} text text_type_main-default`}
+              >
+                {ingredient.name}
+              </p>
+              <div className={`${styles["ingredient-price"]} mr-6`}>
+                <TotalPrice
+                  price={ingredient.price}
+                  double={ingredient.type === "bun" ? true : undefined}
+                />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className={styles["order-date-price"]}>
+        <p>{formatDisplayDate(new Date(orderData.createdAt))}</p>
+        <TotalPrice price={totalPrice} />
+      </div>
+    </div>
   );
 }
