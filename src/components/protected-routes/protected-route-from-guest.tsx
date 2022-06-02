7da8cd60f451f56bb1787f14df40a228d900179e
Maslomeister <maslomeister@gmail.com>
2022-06-02 18:03:18 +0300

    fix: Отображение модалки при рефреше

--- src/components/protected-routes/protected-route-from-guest.tsx
+++ src/components/protected-routes/protected-route-from-guest.tsx
@@ -8,11 +8,15 @@ import { userAuthorized, isTokenExpired, tokenExists } from "../../utils/utils";
 export function ProtectedRouteFromGuest({ children }: RouteProps) {
   let content = children;
   const dispatch = useAppDispatch();
-  const { user, tokens } = useAppSelector((state) => state.authUser);
+  const { user, tokens, status } = useAppSelector((state) => state.authUser);
   const location = useLocation();
 
   if (tokenExists(tokens.refreshToken) && isTokenExpired(tokens.accessToken)) {
     dispatch(getNewAccessToken(tokens.refreshToken));
+  }
+
+  if (status === "getUserData/loading") {
+    return <></>;
   }
 
   if (!userAuthorized(user)) {
