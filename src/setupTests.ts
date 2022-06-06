3911e57eeb9e5f93654cc97b217a1ada586a4abf
Maslomeister <maslomeister@gmail.com>
2022-06-06 17:04:00 +0300

    feat: Инициализая тестов

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -5,6 +5,6 @@
 import "@testing-library/jest-dom";
 
 import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-17";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 Enzyme.configure({ adapter: new Adapter() });
