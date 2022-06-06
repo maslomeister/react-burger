743c02d4f5080fcbf9f9d53ce84e8e3eaff04abb
Maslomeister <maslomeister@gmail.com>
2022-06-06 22:54:11 +0300

    feat: Анимированная dropdown иконка

---
+++ src/components/app-header/components/drop-down-icon/drop-down-icon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+import {
+  ArrowUpIcon,
+  ArrowDownIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+
+type TProps = {
+  isOpened: boolean;
+};
+
+export function DropDownIcon({ isOpened }: TProps) {
+  return (
+    <motion.div
+      initial={{ rotate: 0 }}
+      animate={{ rotate: isOpened ? -180 : 0 }}
+    >
+      <ArrowDownIcon type="primary" />
+    </motion.div>
+  );
+}
