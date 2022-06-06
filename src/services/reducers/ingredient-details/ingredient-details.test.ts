4ef5c5db7d9b9da5cb2f3b07f19a422a78ed6973
Maslomeister <maslomeister@gmail.com>
2022-06-06 20:38:47 +0300

    feat: Тесты для модалки

---
+++ src/services/reducers/ingredient-details/ingredient-details.test.ts
@@ -0,0 +1,28 @@
+import reducer, {
+  initialState,
+  ISliceState,
+  addDataToModal,
+  resetModalData,
+} from "./ingredient-details";
+
+const data: ISliceState = {
+  modalImage: "https://code.s3.yandex.net/react/code/bun-01-large.png",
+  modalName: "Флюоресцентная булка R2-D3",
+  modalCalories: 643,
+  modalProteins: 988,
+  modalFat: 26,
+  modalCarbohydrates: 85,
+};
+
+test("Should add data to modal", () => {
+  expect(reducer(initialState, addDataToModal(data))).toEqual({
+    ...data,
+    showModal: true,
+  });
+});
+
+test("Should reset modal data", () => {
+  expect(reducer({ ...data, showModal: true }, resetModalData())).toEqual(
+    initialState
+  );
+});
