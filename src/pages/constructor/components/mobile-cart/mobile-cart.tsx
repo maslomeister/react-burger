e553fc7ece0a4827e0b97e0e2e2c55ab2698b1b5
Maslomeister <maslomeister@gmail.com>
2022-06-07 22:16:47 +0300

    feat: Корзина на мобильной версии

---
+++ src/pages/constructor/components/mobile-cart/mobile-cart.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./mobile-cart.module.css";
+
+type Props = {};
+
+export function MobileCart({}: Props) {
+  return <div className={styles["mobile-cart"]}>mobile-cart</div>;
+}
