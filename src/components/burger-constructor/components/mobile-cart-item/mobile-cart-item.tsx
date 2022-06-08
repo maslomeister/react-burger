cc259ce79a2544088b867062a1222952a121c953
Maslomeister <maslomeister@gmail.com>
2022-06-08 23:35:18 +0300

    feat: Ингредиент для телефона

---
+++ src/components/burger-constructor/components/mobile-cart-item/mobile-cart-item.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { DragIcon } from "../../../../assets/icons/drag-icon";
+import { TotalPrice } from "../../../total-price/total-price";
+import styles from "./mobile-cart-item.module.css";
+
+interface IProps {
+  image: string;
+  name: string;
+  price: number;
+}
+
+export function MobileCartItem({ image, name, price }: IProps) {
+  return (
+    <div className={styles["mobile-cart-item-inner"]}>
+      <img
+        className={styles["mobile-cart-image"]}
+        src={image}
+        alt="ingredient preview"
+      />
+      <p
+        className={`${styles["mobile-cart-name"]} text text_type_main-default`}
+      >
+        {name}
+      </p>
+      <TotalPrice price={price} />
+    </div>
+  );
+}
--- src/pages/constructor/components/mobile-cart/mobile-cart.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import styles from "./mobile-cart.module.css";
-
-type Props = {};
-
-export function MobileCart({}: Props) {
-  return <div className={styles["mobile-cart"]}>mobile-cart</div>;
-}
