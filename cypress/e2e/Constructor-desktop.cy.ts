26d2f349123c3061043828ed472cf607b2b23d7e
Maslomeister <maslomeister@gmail.com>
2022-06-10 21:01:09 +0300

    feat: E2E тесты

--- cypress/e2e/Constructor-desktop.cy.ts
+++ cypress/e2e/Constructor-desktop.cy.ts
@@ -4,47 +4,47 @@ describe("App desktop constructor E2E tests", () => {
     cy.viewport(1280, 720);
   });
 
-  // it("Should open and close ingredient info modal", () => {
-  //   cy.get('[data-testid="60d3b41abdacab0026a733c6"]').click();
+  it("Should open and close ingredient info modal", () => {
+    cy.get('[data-testid="60d3b41abdacab0026a733c6"]').click();
 
-  //   cy.get('[data-testid="modal-close-icon"]', { timeout: 1000 }).should(
-  //     "be.visible"
-  //   );
+    cy.get('[data-testid="modal-close-icon"]', { timeout: 1000 }).should(
+      "be.visible"
+    );
 
-  //   cy.get('[data-testid="modal-close-icon"]').click();
+    cy.get('[data-testid="modal-close-icon"]').click();
 
-  //   cy.get('[data-testid="modal-close-icon"]').should("not.exist");
-  // });
+    cy.get('[data-testid="modal-close-icon"]').should("not.exist");
+  });
 
-  // it("Should drop bun to constructor and then remove it", () => {
-  //   cy.get('[data-testid="60d3b41abdacab0026a733c6"]').drag(
-  //     '[data-testid="constructor-drop-target"]'
-  //   );
+  it("Should drop bun to constructor and then remove it", () => {
+    cy.get('[data-testid="60d3b41abdacab0026a733c6"]').drag(
+      '[data-testid="constructor-drop-target"]'
+    );
 
-  //   cy.get('[data-testid="top60d3b41abdacab0026a733c6"]').should("be.visible");
+    cy.get('[data-testid="top60d3b41abdacab0026a733c6"]').should("be.visible");
 
-  //   cy.get('[data-testid="top60d3b41abdacab0026a733c6"]')
-  //     .find('*[class^="constructor-element__action"]')
-  //     .click();
+    cy.get('[data-testid="top60d3b41abdacab0026a733c6"]')
+      .find('*[class^="constructor-element__action"]')
+      .click();
 
-  //   cy.get('[data-testid="top60d3b41abdacab0026a733c6"]').should("not.exist");
-  // });
+    cy.get('[data-testid="top60d3b41abdacab0026a733c6"]').should("not.exist");
+  });
 
-  // it("Should drop ingredient to constructor and remove it", () => {
-  //   cy.get('[data-testid="60d3b41abdacab0026a733cd"]').drag(
-  //     '[data-testid="constructor-drop-target"]'
-  //   );
+  it("Should drop ingredient to constructor and remove it", () => {
+    cy.get('[data-testid="60d3b41abdacab0026a733cd"]').drag(
+      '[data-testid="constructor-drop-target"]'
+    );
 
-  //   cy.get('[data-testid="inner60d3b41abdacab0026a733cd"]').should(
-  //     "be.visible"
-  //   );
+    cy.get('[data-testid="inner60d3b41abdacab0026a733cd"]').should(
+      "be.visible"
+    );
 
-  //   cy.get('[data-testid="inner60d3b41abdacab0026a733cd"]')
-  //     .find('*[class^="constructor-element__action"]')
-  //     .click();
+    cy.get('[data-testid="inner60d3b41abdacab0026a733cd"]')
+      .find('*[class^="constructor-element__action"]')
+      .click();
 
-  //   cy.get('[data-testid="inner60d3b41abdacab0026a733cd"]').should("not.exist");
-  // });
+    cy.get('[data-testid="inner60d3b41abdacab0026a733cd"]').should("not.exist");
+  });
 
   it("Should place order if user is not logged in", () => {
     cy.get('[data-testid="60d3b41abdacab0026a733c6"]').drag(
@@ -79,29 +79,29 @@ describe("App desktop constructor E2E tests", () => {
     cy.get('[data-testid="modal-close-icon"]').click();
   });
 
-  it("Should place order if user is logged in", () => {
-    cy.get('[data-testid="60d3b41abdacab0026a733c6"]').drag(
-      '[data-testid="constructor-drop-target"]'
-    );
+  // it("Should place order if user is logged in", () => {
+  //   cy.get('[data-testid="60d3b41abdacab0026a733c6"]').drag(
+  //     '[data-testid="constructor-drop-target"]'
+  //   );
 
-    cy.get('[data-testid="top60d3b41abdacab0026a733c6"]').should("be.visible");
+  //   cy.get('[data-testid="top60d3b41abdacab0026a733c6"]').should("be.visible");
 
-    cy.get('[data-testid="60d3b41abdacab0026a733cd"]').drag(
-      '[data-testid="constructor-drop-target"]'
-    );
+  //   cy.get('[data-testid="60d3b41abdacab0026a733cd"]').drag(
+  //     '[data-testid="constructor-drop-target"]'
+  //   );
 
-    cy.get('[data-testid="inner60d3b41abdacab0026a733cd"]').should(
-      "be.visible"
-    );
+  //   cy.get('[data-testid="inner60d3b41abdacab0026a733cd"]').should(
+  //     "be.visible"
+  //   );
 
-    cy.log(Cypress.env("username"));
+  //   cy.log(Cypress.env("username"));
 
-    cy.get('[data-testid="place-order"]').click();
+  //   cy.get('[data-testid="place-order"]').click();
 
-    cy.get('[data-testid="placed-order-number"]', { timeout: 30000 }).should(
-      "be.visible"
-    );
+  //   cy.get('[data-testid="placed-order-number"]', { timeout: 30000 }).should(
+  //     "be.visible"
+  //   );
 
-    cy.get('[data-testid="modal-close-icon"]').click();
-  });
+  //   cy.get('[data-testid="modal-close-icon"]').click();
+  // });
 });
