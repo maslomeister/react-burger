8ba8143a92b6462501141c1a0eca672320ffc40b
Maslomeister <maslomeister@gmail.com>
2022-06-11 21:27:03 +0300

    feat: Тест авторизованного заказа

---
+++ cypress/e2e/desktop/Constructor-authorized.cy.ts
@@ -0,0 +1,46 @@
+describe("Desktop constructor E2E authorized tests", () => {
+  beforeEach(() => {
+    cy.viewport(1280, 720);
+    cy.session(
+      "logged-in",
+      () => {
+        cy.visit("/login");
+        cy.get('input[name="email"]').type(Cypress.env("email"));
+        cy.get('input[name="password"]').type(Cypress.env("password"));
+        cy.get('button[type="submit').click();
+
+        cy.url().should("contain", "/");
+
+        cy.wait(1500);
+
+        cy.getCookie("accessToken").get("value").should("not.be.empty");
+        cy.getCookie("refreshToken").get("value").should("not.be.empty");
+      },
+      {
+        validate() {
+          cy.getCookie("accessToken").get("value").should("not.be.empty");
+          cy.getCookie("refreshToken").get("value").should("not.be.empty");
+        },
+      }
+    );
+    cy.visit("/");
+  });
+
+  it("Should be able to place order", () => {
+    cy.get('[data-test-id="60d3b41abdacab0026a733c6"]').drag(
+      '[data-test-id="constructor-drop-target"]'
+    );
+
+    cy.get('[data-test-id="60d3b41abdacab0026a733cd"]').drag(
+      '[data-test-id="constructor-drop-target"]'
+    );
+
+    cy.get('[data-test-id="place-order"]').click();
+
+    cy.get('[data-test-id="placed-order-number"]', { timeout: 30000 }).should(
+      "be.visible"
+    );
+
+    cy.get('[data-test-id="modal-close-icon"]').click();
+  });
+});
